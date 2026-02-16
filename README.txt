d6614aecd011b1d817f05a73e5c9e7e1bd0b9d81
Greatdev655 <greatiyke7@gmail.com>
2026-02-16 23:56:02 +0100

    creating a landing page for zentry studio

---
+++ README.txt
@@ -0,0 +1,2 @@
+creating a landing page for Zentry studio . the website is 
+meant to connect the firm to its customers. the bre
---
+++ script.js
@@ -0,0 +1,20 @@
+// reveal animation
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    const reveals = document.querySelectorAll('.reveal');
+
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 100;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add('active');
+        }
+    }
+}
+
+
+
